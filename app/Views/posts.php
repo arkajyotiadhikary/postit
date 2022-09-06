61fd146d842f0942602c6a93ea78459bd1682d38
Arka Jyoti Adhikary <arkajyotiadhikary15@gmail.com>
2022-09-07 02:33:17 +0530

    post buttons

--- app/Views/posts.php
+++ app/Views/posts.php
@@ -2,26 +2,28 @@
 <?= $this->section('body') ?>
 <div class="container h-100 w-100 ">
     <h1 class="text-center my-5">Posts</h1>
-    <div class="w-100 d-flex justify-content-center">
-        <button class="btn btn-primary selected rounded-0">All Posts</button>
-        <button class="btn rounded-0">My Posts</button>
+    <div class="post-btn-group w-100 d-flex justify-content-center">
+        <button name="all-post" id="selected-btn" class="btn all-post rounded-0" onclick="handlePostBtn(this)">All Posts</button>
+        <button name="my-post" id="" class="btn my-post rounded-0" onclick="handlePostBtn(this)">My Posts</button>
         <a href="/postit/post" class="btn btn-success rounded-0 text-white">Add Post</a>
     </div>
     <div class="d-flex justify-content-start">
-        <!--        <ul class="post-list nav w-100">
-        <?php if ($posts): ?>
-            <?php foreach ($posts as $post): ?>
-                                                                    <li class="nav-item border p-4 my-3 w-100">
-                                                                        <h2><?= $post['post_title'] ?></h2>
-                                                                        <p><?= $post['post'] ?></p>
-                                                                        <div class="d-flex w-100">
-                                                                            <p class='mr-3'><?= $post['author'] ?></p>
-                                                                            <p><?= explode(" ", $post['post_date'])[0] ?></p>
-                                                                        </div>
-                                                                    </li> 
-            <?php endforeach; ?>
-        <?php endif; ?>
-                </ul>-->
+        <!-- All Post-->
+        <ul class="post-list nav w-100">
+            <?php if ($posts): ?>
+                <?php foreach ($posts as $post): ?>
+                    <li class="nav-item border p-4 my-3 w-100">
+                        <h2><?= $post['post_title'] ?></h2>
+                        <p><?= $post['post'] ?></p>
+                        <div class="d-flex w-100">
+                            <p class='mr-3'><?= $post['author'] ?></p>
+                            <p><?= explode(" ", $post['post_date'])[0] ?></p>
+                        </div>
+                    </li> 
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </ul>
+        <!-- My Post-->
         <ul class="mypost-list nav w-100">
             <?php if ($myposts): ?>
                 <?php foreach ($myposts as $post): ?>
--- public/assets/index.js
+++ public/assets/index.js
@@ -6,6 +6,23 @@ function handleClick(e) {
     }
 }
 
-function onSubmit(e){
-    
+function handlePostBtn(e) {
+    const post_list = document.querySelector(".post-list");
+    const mypost_list = document.querySelector(".mypost-list");
+    const all_post_btn = document.querySelector(".all-post");
+    const my_post_btn = document.querySelector(".my-post");
+
+
+    if (e.name === "all-post") {
+        all_post_btn.id = "selected-btn";
+        my_post_btn.id = "";
+
+        post_list.style.display = "block";
+        mypost_list.style.display = "none";
+    } else {
+        all_post_btn.id = "";
+        my_post_btn.id = "selected-btn";
+        post_list.style.display = "none";
+        mypost_list.style.display = "block";
+    }
 }
